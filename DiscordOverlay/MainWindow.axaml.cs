7770e4046a16e69781521e4eec9fc3eeeaf361a3
Sean-Salamah <seanhsalamah@gmail.com>
2026-04-25 15:11:36 +0100

    Project created and background made transaparent

---
+++ DiscordOverlay/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+
+namespace DiscordOverlay;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    } 
+    
+    public partial class StackPanel : Panel;
+    
+}
